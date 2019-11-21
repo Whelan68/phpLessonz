4f0ccceab97c9fc567cebb3cfa91b89266164c0b
Whelan68 <54954642+Whelan68@users.noreply.github.com>
2019-11-21 11:29:57 -0800

    Create calcForm.php

---
+++ calcForm.php
@@ -0,0 +1,18 @@
+<head>
+		<style>
+			#peeps {
+				color:red;
+			}
+		</style>
+	</head>
+	<body>
+		<form action="runCalc.php" method="get">
+			Num1: <input type="text" name="num1"><br>
+			Num2: <input type="text" name="num2"><br>
+			<select name="type">
+				<option>add</option>
+				<option>subtract</option>
+			<input type="submit" value="Calculate">
+		</form>
+	</body>
+</html>
