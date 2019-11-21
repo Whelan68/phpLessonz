3d6381c509712c80bbb7e8a70905820e27478095
Whelan68 <54954642+Whelan68@users.noreply.github.com>
2019-11-21 10:16:05 -0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<style>
+			#peeps {
+				color:red;
+			}
+		</style>
+	</head>
+	<body>
+		<?php
+			$first="Hello World";
+			echo $first;
+			echo "<h1 id='peeps'>".$first."</h1>";
+		?>
+	</body>
+</html>
