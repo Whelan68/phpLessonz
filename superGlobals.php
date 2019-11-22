a50f40ace3fd5aeadc34fc71f59354f89ad72582
Whelan68 <54954642+Whelan68@users.noreply.github.com>
2019-11-22 11:01:22 -0800

    Create superGlobals.php

---
+++ superGlobals.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<style>
+			#peeps {
+				color:red;
+			}
+		</style>
+	</head>
+	<body>
+		<?php	
+			echo "Hello World - from: ".$_SERVER['SERVER_SOFTWARE'];
+			echo "<h1 id='peeps'>".$_SERVER['HTTP_HOST']."</h1>";
+			
+			$testArray[] = array('test1','test2');
+		?>
+	</body>
+</html>
