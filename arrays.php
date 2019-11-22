79caf22f59d327ca6f0fec0843c5db0d63497843
Whelan68 <54954642+Whelan68@users.noreply.github.com>
2019-11-22 11:01:44 -0800

    Create arrays.php

---
+++ arrays.php
@@ -0,0 +1,17 @@
+<html>
+	<head>
+		<style>
+			#peeps {
+				color:red;
+			}
+		</style>
+	</head>
+	<body>
+		<?php	
+			$testArray = array('test1','test2');
+			foreach($testArray as $test){
+				echo $test;
+			}
+		?>
+	</body>
+</html>
