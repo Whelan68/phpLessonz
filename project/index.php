b1fe46360cb4cb15a16ddebbac1f9bb9e7d08b7b
Whelan68 <54954642+Whelan68@users.noreply.github.com>
2019-12-03 11:09:19 -0800

    Update index.php

--- project/index.php
+++ project/index.php
@@ -1,1 +1,45 @@
-
+<html>
+	<head>
+		<style>
+			body{
+				background-color:darkblue;
+				color:white
+				}
+			#title{
+				text-align: center;
+			}
+			#bigDiv{
+				margin: auto;
+				width: 75%;
+				border: 3px solid white;
+			}
+			#midDiv{
+				margin: auto;
+				width: 90%;
+			}
+			#dir{
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<h1 id="title">Home Page</h1>
+		<div id="bigDiv">
+			<h3 id="dir">Choose a file</h3>
+			<div id="midDiv">
+				<div>test.php
+				</div>
+				<div>form.php
+				</div>
+				<div>runGet.php
+				</div>
+				<div>runPost.php
+				</div>
+				<div>calcForm.php</div>
+			</div>
+		</div>
+		<style>
+			
+		</style>
+	</body>
+</html>
